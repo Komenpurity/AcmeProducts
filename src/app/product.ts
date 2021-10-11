44bb25e9f7723d9ee1d5bbd9fe6f133e5659b389
Komenpurity <79756168+Komenpurity@users.noreply.github.com>
2021-10-11 08:37:55 +0300

    added a directive

--- src/app/product.ts
+++ src/app/product.ts
@@ -1,2 +1,12 @@
 export class Product {
+  
+  productId!: number;
+  productName!: string;
+  productCode!: string;
+  releaseDate!: string;
+  price!: number;
+  description!: string;
+  starRating!: number;
+  imageUrl!: string;
+
 }
