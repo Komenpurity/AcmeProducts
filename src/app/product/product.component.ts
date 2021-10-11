c307f1d294783f1856d3bcaecf4ec368bb1676de
Komenpurity <79756168+Komenpurity@users.noreply.github.com>
2021-10-11 08:09:53 +0300

    added title

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
+  pageTitle: string = "Acme product management";
+  
   constructor() { }
 
   ngOnInit(): void {
